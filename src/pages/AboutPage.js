1bab74e33ab506d77181e9a9575caa2f70236ef9
AndjelaS27 <stojkovicandjela157@gmail.com>
2025-02-12 21:21:04 +0100

    changing video url

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,7 +8,13 @@ function AboutPage ({selectedLanguage}) {
                 <p className='bg-[rgba(196,35,72,0.8)] md:bg-transparent rounded-xl p-2 w-[85vw] md:w-[30vw] text-[#EDE0CF] aboutFont text-4xl' dangerouslySetInnerHTML={{ __html: languages[selectedLanguage].quote}}/>
             </div>
             <div>
-                <video src='videos/aboutVideo.mp4' autoPlay loop muted className='absolute left-0 top-0 size-full object-cover object-center'/>
+                <video 
+                    src='https://www.dropbox.com/scl/fi/tqy48fm9b92yum352x5h2/aboutVideo.mp4?rlkey=cg4xqpy1dlfolbfiet0wgk829&st=18ww993c&dl=0&raw=1' 
+                    autoPlay 
+                    loop 
+                    muted 
+                    className='absolute left-0 top-0 size-full object-cover object-center'
+                />
             </div>
             <div className='md:w-[33vw] z-40 mt-5 md:mr-5 flex flex-wrap md:flex-col justify-between text-white px-5 md:px-0'>
                 <div className='flex mb-5 md:mb-0'>
